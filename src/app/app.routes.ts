1b124f2256cf87e0a12db8765a3eddfdff63ef2b
jlescan0 <jlescanog1@upao.edu.pe>
2024-09-18 00:16:47 -0500

    asdsadasd

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { RegistroComponent } from './features/registro/registro.component';
 import { LoginComponent } from './features/login/login.component';
+import { RecuperarPasswordComponent } from './features/recuperar-password/recuperar-password.component';
 
 export const routes: Routes = [
     {path: 'registro', component: RegistroComponent},
     {path: 'login', component: LoginComponent},
+    {path: 'recuperar', component: RecuperarPasswordComponent},
 ];
